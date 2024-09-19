1f8d5f509da07c9c2de44824404af61982a36f49
ristukaJJang <ghwjd5585@gmail.com>
2024-09-19 17:05:19 +0900

    09-19 17:05

--- src/main/java/com/korit/senicare/entity/ToolEntity.java
+++ src/main/java/com/korit/senicare/entity/ToolEntity.java
@@ -1,6 +1,8 @@
 package com.korit.senicare.entity;
 
 
+import com.korit.senicare.dto.request.tool.PostToolRequestDto;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,4 +27,10 @@ public class ToolEntity {
     private String name;
     private String purpose;
     private Integer count;
+
+    public ToolEntity(PostToolRequestDto dto) {
+        this.name = dto.getName();
+        this.purpose = dto.getPurpose();
+        this.count = dto.getCount();
+    }
 }
